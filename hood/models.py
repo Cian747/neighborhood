e709778f62d788953724aabdead2fca059161b8c
Cian <cian.omondi@student.moringaschool.com>
2021-09-23 21:45:09 +0300

    Created models

--- hood/models.py
+++ hood/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.urls import reverse
 
 # Create your models here.
 
 class Neighborhood(models.Model):
     neighborhood_name = models.CharField(max_length=100)
+    image = CloudinaryField('neighborhood image',blank=True,null=True)
     location = models.CharField(max_length=50)
-    occupants = models.IntegerField(default=0)
+    occupants = models.IntegerField(default=0,null=True,blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        self.neighborhood_name
+    # def __str__(self) -> str:
+    #     self.neighborhood_name
 
     def save_neighborhood(self):
         self.save()
@@ -21,19 +23,30 @@ class Neighborhood(models.Model):
 
     @classmethod
     def update_neighborhood_name(cls, id,name):
-        return cls.objects.filter(id = id).update(name=name)
+        return cls.objects.filter(id = id).update(neighborhood_name=name)
+
+    @classmethod
+    def update_neighborhood_occupants(cls, id,occ):
+        return cls.objects.filter(id = id).update(occupants=occ)
+
+    @classmethod
+    def search_neighborhood(cls, search_term):
+        return cls.objects.filter(neighborhood_name__icontains = search_term)
+
+
+
+    def __str__(self) -> str:
+       return self.neighborhood_name
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    family_name = models.CharField(max_length=50,null=True,blank=True)
     profile_photo = CloudinaryField('image',blank=True,null=True)
     hood = models.ForeignKey(Neighborhood, on_delete=models.DO_NOTHING,null=True)
     family_email = models.EmailField(blank=True,null=True)
     family_members = models.IntegerField(null=True,blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    
-    def __str__(self):
-        self.user.username
 
     def save_profile(self):
         self.save()
@@ -42,19 +55,33 @@ class Profile(models.Model):
         self.delete()
 
     @classmethod
-    def update_profile_photo(cls, id,profile_photo):
-        return cls.objects.filter(id = id).update(profile_photo=profile_photo)
+    def update_profile_photo(cls, id,family_email):
+        return cls.objects.filter(id = id).update(family_email=family_email)
+
+    
+    @classmethod
+    def search_username(cls,search_term):
+        return cls.objects.filter(user__username__icontains = search_term)
 
 
+    def get_absolute_url(self):
+        return reverse('profile',args=[str(self.id)])
+
+        # return reverse('home')
+    
+
+
+    def __str__(self) -> str:
+       return self.user.username
+
+# Add the messages on the page of each 
 class Meeting(models.Model):
+    title = models.CharField(max_length=30)
     content = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     hood = models.ForeignKey(Neighborhood,on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        self.user.username
 
     def save_meeting(self):
         self.save()
@@ -62,15 +89,22 @@ class Meeting(models.Model):
     def delete_meeting(self):
         self.delete()
 
+    @classmethod
+    def update_meeting_title(cls, id,title):
+        return cls.objects.filter(id = id).update(title=title)
+
+
+    def __str__(self) -> str:
+        return self.user.username
+
 
 class Business(models.Model):
     name = models.CharField(max_length=50)
+    image = CloudinaryField('Business photo',blank=True,null=True)
+    description = models.TextField()
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     neighbor = models.ForeignKey(Neighborhood,on_delete=models.DO_NOTHING)
     business_email = models.EmailField()
-
-    def __str__(self):
-        self.name
 
     def save_business(self):
         self.save()
@@ -79,8 +113,18 @@ class Business(models.Model):
         self.delete()
 
     @classmethod
+    def search_business(cls,search_term):
+        return cls.objects.filter(name__icontains = search_term)
+
+    @classmethod
     def update_business_name(cls, id,name):
         return cls.objects.filter(id = id).update(name=name)
+ 
+    def __str__(self) -> str:
+        return self.name
+
+
+
 
 
     
